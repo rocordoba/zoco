ec4aa583f008060762be5a85de3cf2aeba979057
ZocoIT <154919337+ZocoIT@users.noreply.github.com>
2023-12-27 11:25:31 -0300

    Comparativas
    se le agrego el mes a las comparativas para que ademas del total se mande el mes que se compara

--- Aliados/DAL/obj/Debug/net6.0/DAL.AssemblyInfo.cs
+++ Aliados/DAL/obj/Debug/net6.0/DAL.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DAL")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9fb122a6d70b044494a10455d908354e1f0e1291")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("DAL")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DAL")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

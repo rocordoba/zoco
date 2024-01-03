3f563329899b385fd9bc01db20c1651de6ec457b
dev <zocoweb.com.ar@gmail.com>
2024-01-03 16:50:59 -0300

    utilizo el contexto en inicio

--- Aliados/dash-aliados/ClientApp/src/views/Inicio.js
+++ Aliados/dash-aliados/ClientApp/src/views/Inicio.js
@@ -12,61 +12,67 @@ import ScrollToTopButton from "../components/ScrollToTopButton";
 import TituloPagina from "../components/TituloPagina";
 // import ModalEditable from "../components/ModalEditable";
 import PopUpCalificar from "../components/PopUpCalificar";
+import { DatosInicioContext } from "../context/DatosInicioContext";
+
+
 
 const Inicio = ({ califico, setCalifico }) => {
   const { darkMode } = useContext(DarkModeContext);
-  const [datosBack, setDatosBack] = useState({});
+
+  const {datosBackContext, setDatosBackContext} = useContext(DatosInicioContext)
+  console.log("🚀 ~ file: Inicio.js:22 ~ Inicio ~ datosBackContext:", datosBackContext)
+  // const [datosBack, setDatosBack] = useState({});
   const [contador, setContador] = useState(0);
   const [datosMandados, setDatosMandados] = useState();
 
 
-  useEffect(() => {
-    const token = localStorage.getItem("token");
-    const userId = localStorage.getItem("userId");
-    const currentDate = new Date();
-    const year = currentDate.getFullYear();
-    const month = currentDate.getMonth() + 1; 
-    const week = Math.ceil(currentDate.getDate() / 7); 
-    const comercio = "Todos";
-    const day = currentDate.getDay();
-    const requestData = {
-      token: token,
-      id: userId,
-      year: year,
-      month: month,
-      week: week,
-      comercio: comercio,
-      day: day,
-    };
+  // useEffect(() => {
+  //   const token = localStorage.getItem("token");
+  //   const userId = localStorage.getItem("userId");
+  //   const currentDate = new Date();
+  //   const year = currentDate.getFullYear();
+  //   const month = currentDate.getMonth() + 1; 
+  //   const week = Math.ceil(currentDate.getDate() / 7); 
+  //   const comercio = "Todos";
+  //   const day = currentDate.getDay();
+  //   const requestData = {
+  //     token: token,
+  //     id: userId,
+  //     year: year,
+  //     month: month,
+  //     week: week,
+  //     comercio: comercio,
+  //     day: day,
+  //   };
 
-    if (token && userId) {
-      fetch(`/api/datosinicio/base`, {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify(requestData),
-      })
-        .then((response) => {
-          if (!response.ok) {
-            throw new Error("");
-          }
-          return response.json();
-        })
-        .then((data) => {
-          setDatosBack(data);
-        })
-        .catch((error) => {
-          console.error("Error en la solicitud:", error);
-        });
-    }
-  }, []);
+  //   if (token && userId) {
+  //     fetch(`/api/datosinicio/base`, {
+  //       method: "POST",
+  //       headers: {
+  //         "Content-Type": "application/json",
+  //       },
+  //       body: JSON.stringify(requestData),
+  //     })
+  //       .then((response) => {
+  //         if (!response.ok) {
+  //           throw new Error("");
+  //         }
+  //         return response.json();
+  //       })
+  //       .then((data) => {
+  //         setDatosBack(data);
+  //       })
+  //       .catch((error) => {
+  //         console.error("Error en la solicitud:", error);
+  //       });
+  //   }
+  // }, []);
 
 
   return (
     <div>
       <div className="d-xl-block d-none mt-4 pt-4 ">
-        <BienvenidoPanel datos={datosBack} />
+        <BienvenidoPanel datos={datosBackContext} />
       </div>
       <ScrollToTopButton />
       <div className="pt-2">
@@ -74,7 +80,7 @@ const Inicio = ({ califico, setCalifico }) => {
       </div>
 
       <div className="my-3">
-        <DatosInicio datos={datosBack} />
+        <DatosInicio datos={datosBackContext} />
       </div>
       {/* <ModalEditable /> */}
       {califico === 1 && (
@@ -83,16 +89,16 @@ const Inicio = ({ califico, setCalifico }) => {
         </>
       )}
       <div className="pb-4">
-        <ComportamientoGrafica datos={datosBack} />
+        <ComportamientoGrafica datos={datosBackContext} />
       </div>
       <div className="d-none d-xl-block">
-        <GraficaData datos={datosBack} />
+        <GraficaData datos={datosBackContext} />
       </div>
       <div className="d-xl-none d-block">
         {darkMode ? (
-          <GraficaDataCelularDark datos={datosBack} />
+          <GraficaDataCelularDark datos={datosBackContext} />
         ) : (
-          <GraficaDataCelular datos={datosBack} />
+          <GraficaDataCelular datos={datosBackContext} />
         )}
       </div>
       <div className="py-4">

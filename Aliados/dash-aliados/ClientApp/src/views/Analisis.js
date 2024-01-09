2bddaf2e25e2024589dd28396363fd32dca453ef
dev <zocoweb.com.ar@gmail.com>
2024-01-09 11:08:11 -0300

    resuelvo los conflictos del merge

--- Aliados/dash-aliados/ClientApp/src/views/Analisis.js
+++ Aliados/dash-aliados/ClientApp/src/views/Analisis.js
@@ -9,54 +9,9 @@ import TituloPagina from "../components/TituloPagina";
 import { DatosInicioContext } from "../context/DatosInicioContext";
 
 const Analisis = () => {
-<<<<<<< HEAD
-  const { datosAnalisisContext } = useContext(DatosInicioContext);
-=======
-  const [datosBack, setDatosBack] = useState({});
-  useEffect(() => {
-   
-      const token = localStorage.getItem('token');
-      const userId = localStorage.getItem('userId');
-      const currentDate = new Date();
-      const year = currentDate.getFullYear();
-      const month = currentDate.getMonth() + 1; // Sumar 1 porque los meses van de 0 a 11
-      const week = Math.ceil(currentDate.getDate() / 7); // Obtener la semana actual
-      const comercio = "Todos";
-      const day = currentDate.getDay();
-      const requestData = {
-          token: token,
-          id: userId,
-          year: year,
-          month: month,
-          week: week,
-          comercio: comercio,
-          day: day,
-      };
 
-    if (token && userId) {
-      fetch(`/api/analisis/analisis`, {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify(requestData),
-      })
-        .then((response) => {
-          if (!response.ok) {
-            throw new Error("");
-          }
-          return response.json();
-        })
-        .then((data) => {
-          console.log("Datos recuperados:", data);
-          setDatosBack(data);
-        })
-        .catch((error) => {
-          console.error("Error en la solicitud:", error);
-        });
-    }
-  }, []);
->>>>>>> 941531e39e46fcbec4eaa6eeeae135b2d02a1370
+  const { datosAnalisisContext } = useContext(DatosInicioContext);
+
 
   return (
     <div>
